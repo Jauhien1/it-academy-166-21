59e96b5bbfd3942f0d234c1204fd4ba670d1dbc5
Dima Astreyko <desfarik@gmail.com>
2021-11-26 18:44:48 +0300

    lesson 15

---
+++ src/d.astreyko/lesson_14/index.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 1000; i++) {
+    console.log(i++);
+}
+
+const answer = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+
+if (answer == 2015) {
+    alert('Вы правы!');
+} else {
+}
+(company == 'Netscape') ?
+    alert('Верно!') :
+    alert('Неправильно.');
